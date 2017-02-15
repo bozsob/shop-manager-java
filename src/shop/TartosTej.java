b1ad1293c3c2d69137f5141420c5544cca81d485
Trixi <bozso.beatrix@gmail.com>
2017-02-15 09:17:12 +0100

    till Task 3.1 done - TartosTej es FeltartosTej classok megirva, Tej class abstract

---
+++ src/shop/TartosTej.java
@@ -0,0 +1,14 @@
+package shop;
+
+import java.util.Date;
+
+/**
+ * Created by trixi on 2017.02.15..
+ */
+public class TartosTej extends Tej
+{
+    public TartosTej(long vonalKod, int urtartalom, String gyarto, Date szavatossagIdo, double zsirtartalom)
+    {
+        super(vonalKod, urtartalom, gyarto, szavatossagIdo, zsirtartalom);
+    }
+}
--- src/shop/Tej.java
+++ src/shop/Tej.java
@@ -5,29 +5,28 @@ import java.util.Date;
 /**
  * Created by trixi on 2017.02.14..
  */
-public class Tej
+public abstract class Tej
 {
-    private long vonalKod;
+
     private final int LITER = 4;
     private final int FELLITER = 2;
     private final int POHAR = 1;
     private final double ZSIROS = 3.6;
     private final double FELZSIROS = 2.8;
 
-    private int urtartalom;
-    private String gyarto;
-    private Date szavatossagIdo;
-    private double zsirtartalom;
-    private long ar;
+    protected long vonalKod;
+    protected int urtartalom;
+    protected String gyarto;
+    protected Date szavatossagIdo;
+    protected double zsirtartalom;
 
-    public Tej(long vonalKod, int urtartalom, String gyarto, Date szavatossagIdo, double zsirtartalom, long ar)
+    public Tej(long vonalKod, int urtartalom, String gyarto, Date szavatossagIdo, double zsirtartalom)
     {
         this.vonalKod = vonalKod;
         this.urtartalom = urtartalom;
         this.gyarto = gyarto;
         this.szavatossagIdo = szavatossagIdo;
         this.zsirtartalom = zsirtartalom;
-        this.ar = ar;
     }
 
     public long getVonalKod()
@@ -67,17 +66,12 @@ public class Tej
         return zsirtartalom;
     }
 
-    public long getAr()
-    {
-        return ar;
-    }
-
     public String toString()
     {
         return ("Űrtartalom: " + getUrtartalom() + "\n"
                 + "Gyártó: " + getGyarto() + "\n"
                 + "Szavatossági idő: " + getSzavatossagIdo() + "\n"
-                + "Zsírtartalom: " + getZsirtartalom() + "\n"
-                +  "Ár: " + getAr() + "\n");
+                + "Zsírtartalom: " + getZsirtartalom() + "\n");
+                //+  "Ár: " + getAr() + "\n");
     }
 }
