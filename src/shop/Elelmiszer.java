64191d3c886f83d7d5a9dd55c4bac51923ff6937
Trixi <bozso.beatrix@gmail.com>
2017-02-15 10:16:51 +0100

    till Task 3.5 done - Elelmiszer class abstract

---
+++ src/shop/Elelmiszer.java
@@ -0,0 +1,27 @@
+package shop;
+
+import java.util.Date;
+
+/**
+ * Created by trixi on 2017.02.15..
+ */
+public abstract class Elelmiszer
+{
+    protected long vonalKod;
+    protected String gyarto;
+    protected Date szavatossagiIdo;
+
+    public Elelmiszer(long vonalKod, String gyarto, Date szavatossagiIdo)
+    {
+        this.vonalKod = vonalKod;
+        this.gyarto = gyarto;
+        this.szavatossagiIdo = szavatossagiIdo;
+    }
+
+    public abstract long getVonalKod();
+    public abstract boolean joMeg();
+    public abstract Date getSzavatossagiIdo();
+    public abstract String toString();
+
+
+}
