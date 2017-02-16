b24085f5cbffe74cb0417e786ce330788c001d29
Trixi <bozso.beatrix@gmail.com>
2017-02-16 11:36:07 +0100

    TejFactory ujTartosTej and ujFeltartosTej methods - return statements modified

--- src/shop/TejFactory.java
+++ src/shop/TejFactory.java
@@ -1,35 +1,44 @@
 package shop;
 
 import shop.aruk.Tej;
+import shop.aruk.tej.FeltartosTej;
+import shop.aruk.tej.TartosTej;
 
 import java.util.Date;
 
 /**
  * Created by trixi on 2017.02.15..
  */
-public abstract class TejFactory
+public class TejFactory
 {
-    /*public Tej getTej(String tejTipus, long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirtartalom)
+    /* public Tej getTej(String tejTipus, long vonalKod, int urtartalom, String gyarto, Date szavatossagiIdo, double zsirTartalom)
     {
         if (tejTipus.equalsIgnoreCase("Tartós")) {
-            return new TartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
+            return new TartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirTartalom);
         }
         else if (tejTipus.equalsIgnoreCase("Féltartós")) {
-            return new FeltartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirtartalom);
+            return new FeltartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirTartalom);
         }
         return  null;
-    }*/
+    } */
 
-    public abstract Tej ujTartosTej(long vonalKod,
+    public Tej ujTartosTej(long vonalKod,
                                     int urtartalom,
                                     String gyarto,
                                     Date szavatossagiIdo,
                                     double zsirTartalom,
-                                    long ar);
-    public abstract Tej ujFeltartosTej(long vonalKod,
+                                    long ar)
+    {
+        return new TartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirTartalom);
+    }
+
+    public Tej ujFeltartosTej(long vonalKod,
                                        int urtartalom,
                                        String gyarto,
                                        Date szavatossagiIdo,
                                        double zsirTartalom,
-                                       long ar);
+                                       long ar)
+    {
+        return new FeltartosTej(vonalKod, urtartalom, gyarto, szavatossagiIdo, zsirTartalom);
+    }
 }
