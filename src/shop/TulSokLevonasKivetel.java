49e6b1c54189636959e24bd4764e7f8c97d4e25a
Trixi <bozso.beatrix@gmail.com>
2017-02-16 10:18:55 +0100

    modified (now extends BoltKivetel)

--- src/shop/TulSokLevonasKivetel.java
+++ src/shop/TulSokLevonasKivetel.java
@@ -3,7 +3,7 @@ package shop;
 /**
  * Created by trixi on 2017.02.16..
  */
-public class TulSokLevonasKivetel extends Exception
+public class TulSokLevonasKivetel extends BoltKivetel
 {
     public TulSokLevonasKivetel(String message){}
 }
